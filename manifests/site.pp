037a13144761619addbb21e7d87107bd12ec0ae6
Claudiu <109045289+bacanuclaudiu@users.noreply.github.com>
2022-07-20 23:06:36 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'Politicians are clowns',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner => 'root',
-  }
-}
+ }

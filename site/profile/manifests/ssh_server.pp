73f384376891f955ae3cd0380ac1b33ed8701da3
Claudiu <109045289+bacanuclaudiu@users.noreply.github.com>
2022-08-11 19:02:15 +0300

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
                 ensure => present,
                 user => 'root',
                 type => 'ssh-rsa',
-                key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDbHQLuqUdeCnQWM+oVgUn9rq50ufDeEVH5U5gcA3HwDsaJeT2FVTn1EmSg9DpkylXj8sgdGJhs2d/TRyUQkVF05LBP2RBb8yn15g2e69lxuH0BE2mlwIIezzgO4C/mcQgKLoivi4afiMcK55yI1kQJVSf+4wwlAxAirjyqx3D3HRCtuBGoxsqrKeE74mKgLIQR22ocj5o2ChnurxW/zpm8435Tz29yQCGINPCh+71A8XPhf+/L6djwrYb1vdhc6AfSWi++KmUWTuoKhY7VLhQyoK4YtS3vFQfzQoA4cmhsNojQ8G/jI7RHHkkqqsSMuRUvn4D4Z+Uts/JQok+/wMsx',
+                key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDkCUuXX9Qssmbo+w4vDKiaCCiSxk3a+Mwg0d+xh+PhnIaBIGwx4+WGcCkw4tpfC77M2LHhJD32KVft7ZFDrF1pQI/UdCdRYPZ0vWvBgYbhlmH2oyNHomEK8yZ8LlXXaYhcw+a5crksiGfNIjxYa6eR3aTlbwZ/ZxW0j7KNJ09kWgzEr7wsVGcY9JdBrJsJYVkO4mhmFd/soz/cJTCl8ojA5NU5l2RqKZf5c4tGp6PlUTGNndVaKj9GYRmqVm0TezaJ6T01FkovYAP4v7CgllNYQSwi5nKM328yWqJXvV0ZGjkGdEk/PIQycUX5A/N6VxgKdlhhbdlBVbZvevl15IXD',
        }
  }    
